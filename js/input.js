f7d2a3f4b1d80bf107ee4fc030bb2934137b10aa
shnek94 <shnek94@gmail.com>
2017-05-01 21:48:39 +0200

    Adding touch events

--- js/input.js
+++ js/input.js
@@ -1,7 +1,11 @@
 var pressed = [];
+var first = 0;
+
 
 window.addEventListener('keydown', handleKeyDown, false);
 window.addEventListener('keyup', handleKeyUp, false); 
+window.addEventListener('touchstart', touchStartEvent, false);
+window.addEventListener('touchend', touchEndEvent, false);
 
 function handleKeyDown(event){
     var index = pressed.indexOf(event.key.toLowerCase());
@@ -19,4 +23,27 @@ function handleKeyUp(event){
         // If we can find the current key being released in the array of currently pressed keys, we remove it from there!
         pressed.splice(index, 1);
     }
+}
+
+function touchStartEvent(event){
+    if(first == 0){
+        pressed.push('d');
+        first += 1;
+    }else if(first == 1){
+        pressed.push(' ');
+        first += 1;
+    }
+    console.log("Start " + first);
+
+}
+
+function touchEndEvent(event){
+    if(first == 2){
+        pressed.splice(' ', 1);
+        first -= 1;
+    }else if(first == 1){
+        pressed.splice('d', 1);
+        first -= 1;
+    }
+    console.log("End " + first);
 }
